44e2b29821ecda26713f29cc9bd13d350c2a9c3c
NLaureano <nicholasray345@gmail.com>
2025-10-23 18:14:55 -0700

    Still must do indexing properly

--- lstore/db.py
+++ lstore/db.py
@@ -1,18 +1,46 @@
 # db.py
 # Only implemented __init__ and create_table for M1
 from lstore.table import Table
+import os
+import pickle
 
 class Database():
 
     def __init__(self):
         self._tables_by_name = {}
+        self.path = None
 
-    # Not required for milestone1
     def open(self, path):
-        pass
+        self.path = path
+        os.makedirs(path, exist_ok=True)
+        manifest_path = os.path.join(path, 'tables.pkl')
+        if os.path.exists(manifest_path):
+            with open(manifest_path, 'rb') as f:
+                table_names = pickle.load(f)
+            for name in table_names:
+                table_file = os.path.join(path, f'table_{name}.pkl')
+                if os.path.exists(table_file):
+                    with open(table_file, 'rb') as tf:
+                        table_data = pickle.load(tf)
+                    table = Table.from_dict(table_data)
+                    self._tables_by_name[name] = table
 
     def close(self):
-        pass
+        if self.path is None:
+            return
+        
+        # First close all indices to prevent WAL file issues
+        for table in self._tables_by_name.values():
+            table.close()
+        
+        manifest_path = os.path.join(self.path, 'tables.pkl')
+        with open(manifest_path, 'wb') as f:
+            pickle.dump(list(self._tables_by_name.keys()), f)
+            
+        for name, table in self._tables_by_name.items():
+            table_file = os.path.join(self.path, f'table_{name}.pkl')
+            with open(table_file, 'wb') as tf:
+                pickle.dump(table.to_dict(), tf)
 
     """
     # Creates a new table
--- lstore/index.py
+++ lstore/index.py
@@ -1,4 +1,7 @@
 import bplustree
+import os
+import tempfile
+
 """
 A data strucutre holding indices for various columns of a table. Key column should be indexd by default, other columns can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
 """
@@ -7,47 +10,78 @@ class Index:
 
     def __init__(self, table):
         # One index for each table. All our empty initially.
-        self.indices = [None] *  table.num_columns
-        self.create_index(table.key) # Create index on key column by default
-        pass
+        self.table = table
+        self.indices = [None] *  self.table.num_columns
+        self.create_index(self.table.key) # Create index on key column by default
+        
 
     """
     # returns the location of all records with the given value on column "column"
     """
-
-    def locate(self, column, value):
-        pass
+    def locate(self, column, value) -> list[int]:
+        tree = self.indices[column]
+        recordLocations = tree.get(value, [])
+        return recordLocations
 
     """
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
     """
-
     def locate_range(self, begin, end, column):
-        pass
+        recordLocations = []
+        tree = self.indices[column]
+        for _, rid in tree.items(begin, end):
+            recordLocations.append(rid)
+        return recordLocations
 
     """
     # Create index on specific column
     """
-
     def create_index(self, column_number):
-            # Create a B+-tree for the specified column
-            # Key size is set to 8 bytes (for integers), value size is 8 bytes (for RIDs)
-            tree = bplustree.BPlusTree(
-                filename=None,  # In-memory tree
-                key_size=8,
-                value_size=8,
-                order=50,
-                serializer=bplustree.serializer.IntSerializer()
-            )
-            # Populate the tree with existing records
-            for rid, record in enumerate(self.table.records):
-                value = record[column_number]
-                tree[value] = rid
-            self.indices[column_number] = tree
+        # Create a dictionary to temporarily store value -> [RIDs] mapping
+        value_to_rids = {}
+        
+        # First pass: collect all RIDs for each value
+        for rid, _ in self.table.page_directory.items():
+            value = self.table.read(rid, column_number)
+            if value not in value_to_rids:
+                value_to_rids[value] = []
+            value_to_rids[value].append(rid)
+        
+        # Create temporary directory for indices if it doesn't exist
+        temp_dir = os.path.join(tempfile.gettempdir(), 'lstore_indices')
+        os.makedirs(temp_dir, exist_ok=True)
+        
+        # Create a unique filename for this index
+        index_file = os.path.join(temp_dir, f'index_{self.table.name}_{column_number}.idx')
+        
+        # Create a B+-tree for the specified column
+        tree = bplustree.BPlusTree(
+            filename=index_file,
+            key_size=8,
+            value_size=1024,  # Increased to store arrays of RIDs
+            order=50,
+            serializer=bplustree.serializer.IntSerializer()
+        )
+        
+        # Second pass: store arrays of RIDs in the tree
+        for value, rids in value_to_rids.items():
+            tree[value] = rids
+            
+        self.indices[column_number] = tree
+        return
 
     """
     # Drop index of specific column
     """
-
     def drop_index(self, column_number):
-        pass
+        if self.indices[column_number] is not None:
+            self.indices[column_number].close()  # Properly close the B+ tree
+            self.indices[column_number] = None
+        return
+    
+    def close_all_indices(self):
+        """Close all B+ trees properly"""
+        for i in range(len(self.indices)):
+            if self.indices[i] is not None:
+                self.indices[i].close()
+                self.indices[i] = None
